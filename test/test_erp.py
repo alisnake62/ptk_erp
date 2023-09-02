40063d3e348e4fa1587c9b0da75107034ad0ca4b
clement <clementsergent@yahoo.fr>
2023-09-02 15:03:09 +0200

    Test 11

--- test/test_erp.py
+++ test/test_erp.py
@@ -31,7 +31,7 @@ fake_legacy_products = [
 class TestERP:
 
     def test_True(self, mocker):
-        assert True # Test 10
+        assert True # Test 11
 
     def test_get_stocks(self, mocker):
 
